1348ecea1f7a1d6fd00e785ed9a8fd08a907780c
kidrek <kidrek@gmail.com>
2024-08-24 17:27:56 +0200

    Update run_task.py

--- run_task.py
+++ run_task.py
@@ -13,7 +13,6 @@ dotenv_path = Path('.env')
 load_dotenv(dotenv_path=dotenv_path)
 
 
-
 class MyHandler(FileSystemEventHandler):
     def on_created(self, event):
         print(f'File {event.src_path} has been created')
@@ -22,24 +21,22 @@ class MyHandler(FileSystemEventHandler):
 
             # Sanitize filename to remove space, etc...
             sanitize_filename = sanitize_file_name(original_filename)
-            move_input_artifacts(event.src_path, f"{ARTIFACT_PATH}/{sanitize_filename}")
-            time.sleep(0.1)
+            if original_filename != sanitize_filename:
+                app.send_task('task_move_file', args=(event.src_path, f"{ARTIFACT_PATH}/{sanitize_filename}",), retry=True)
 
             # 1st task : generate hash
             app.send_task('task_sha256', args=(f"{ARTIFACT_PATH}/{sanitize_filename}",), retry=True)
-
+            
             # 2nd task : Workflow by evidence type
             evidence_type = determine_evidence_type(original_filename)
             if evidence_type == "VELOCIRAPTOR":
                 app.send_task('task_extract_files', args=(f"{ARTIFACT_PATH}/{sanitize_filename}", f"{SCAN_OUTPUT_PATH}/{sanitize_filename}_extracted", env['VELOCIRAPTOR_EVIDENCE_PASSWORD']), retry=True)
-                #task_extract = app.signature('task_extract_files', args=(original_filename, f"{SCAN_OUTPUT_PATH}/{sanitize_filename}_extracted", env['VELOCIRAPTOR_EVIDENCE_PASSWORD']), retry=True)
-                #tasks = (task_extract)()
 
 
 
 def determine_evidence_type(filename):
     # Determine evidence type
-    if re.match(VELOCIRAPTOR_EVIDENCE_PATTERN, filename):
+    if re.match(VELOCIRAPTOR_EVIDENCE_PATTERN, filename, re.IGNORECASE):
         evidence_type = "VELOCIRAPTOR"
         print("Velociraptor evidence")
     else:
@@ -50,23 +47,7 @@ def determine_evidence_type(filename):
 def sanitize_file_name(filename):
     sanitized_name = re.sub(r'[^a-zA-Z0-9._-]', '-', filename).lower()
     return sanitized_name
-
-
-def move_input_artifacts(source_filepath, dst_filepath):
-    mv_command = [
-        'mv', 
-        f'{source_filepath}',
-        f'{dst_filepath}'
-    ]
-
-    try:
-        result = subprocess.run(mv_command, check=True, capture_output=True, text=True)
-        print(f"Move output: {result.stdout}")
-        if result.stderr:
-            print(f"Move error: {result.stderr}")
-    except subprocess.CalledProcessError as e:
-        print(f"Error: {e.stderr}")
-        raise e
+    
 
 if __name__ == "__main__":
     logging.info(f"Running daemon")
@@ -90,6 +71,3 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         observer.stop()
     observer.join()
-
-
-
