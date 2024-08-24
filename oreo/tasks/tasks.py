1e4ee1abf314063c8c68fa8706662c4e6112e98b
kidrek <kidrek@gmail.com>
2024-08-24 17:29:01 +0200

    Update tasks.py

--- oreo/tasks/tasks.py
+++ oreo/tasks/tasks.py
@@ -7,9 +7,14 @@ from . import *             # Need to declare all python scripts in __init__.py
 
 
 @shared_task(bind=True, name='task_extract_files')
-def task_extract_files(self, filepath, output_filepath, password):
+def task_extract_files(self, filepath:str, output_filepath:str, password:str):
     logging.info(f'Task Extract files : Call task')
     run_extract.run(filepath, output_filepath, password, nested=False)
+
+@shared_task(bind=True, name='task_move_file')
+def task_move_file(self, filepath:str, output_filepath:str):
+    logging.info(f'Task MoveFile : Call task')
+    run_move_file.run(filepath, output_filepath)
 
 
 @shared_task(bind=True, name='task_sha256')
