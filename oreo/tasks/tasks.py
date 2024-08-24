b325e9d94f5c516375567911cf1bb69e280e2106
kidrek <kidrek@gmail.com>
2024-08-24 19:37:29 +0200

    Update tasks.py

--- oreo/tasks/tasks.py
+++ oreo/tasks/tasks.py
@@ -1,26 +1,74 @@
 import re
 import hashlib
 import logging
+from pathlib import Path
+
 
 from celery import shared_task
 from . import *             # Need to declare all python scripts in __init__.py
 
+from dotenv import load_dotenv
+from os import environ as env
 
-@shared_task(bind=True, name='task_extract_files')
+## Load variables from .env file
+dotenv_path = Path('.env')
+load_dotenv(dotenv_path=dotenv_path)
+
+@shared_task(bind=True, name='task_extract_files', priority=5)
 def task_extract_files(self, filepath:str, output_filepath:str, password:str):
     logging.info(f'Task Extract files : Call task')
     run_extract.run(filepath, output_filepath, password, nested=False)
 
-@shared_task(bind=True, name='task_move_file')
+
+@shared_task(bind=True, name='task_move_file', priority=0)
 def task_move_file(self, filepath:str, output_filepath:str):
     logging.info(f'Task MoveFile : Call task')
     run_move_file.run(filepath, output_filepath)
 
 
-@shared_task(bind=True, name='task_sha256')
+@shared_task(bind=True, name='task_sha256', priority=2)
 def task_sha256(self, filepath:str):
     logging.info(f'Task SHA256 : Call task')
     run_sha256.sha256_generate(filepath)
     
 
+@shared_task(bind=True, name='task_init', priority=0)
+def task_init(self, filename):
+    logging.info(f'Task INIT : Call task')
+    if not filename.endswith('.sha256'):
+        original_filename = Path(filename).name
 
+        # Sanitize filename to remove space, etc...
+        sanitize_filename = sanitize_file_name(original_filename)
+        if original_filename != sanitize_filename:
+            task_move_file.delay(filename, f"{env['ARTIFACT_PATH']}/{sanitize_filename}",)
+
+        # 1st task : generate hash
+        task_sha256.delay(f"{env['ARTIFACT_PATH']}/{sanitize_filename}",)
+
+        # 2nd task : Workflow by evidence type
+        evidence_type = determine_evidence_type(original_filename)
+        if evidence_type == "VELOCIRAPTOR":
+            task_extract_files.delay(f"{env['ARTIFACT_PATH']}/{sanitize_filename}", f"{env['SCAN_OUTPUT_PATH']}/{sanitize_filename}_extracted", env['VELOCIRAPTOR_EVIDENCE_PASSWORD'])
+
+            # Create a single task
+            """
+            tasks = [app.send_task('task_sha256', args=(f"{ARTIFACT_PATH}/{sanitize_filename}",), retry=True),
+                        app.send_task('task_extract_files', args=(f"{ARTIFACT_PATH}/{sanitize_filename}", f"{SCAN_OUTPUT_PATH}/{sanitize_filename}_extracted", env['VELOCIRAPTOR_EVIDENCE_PASSWORD']), retry=True)]
+            """
+
+
+# Function to replace all characters, except 'a-zA-Z0-9._-' by -
+def sanitize_file_name(filename):
+    sanitized_name = re.sub(r'[^a-zA-Z0-9._-]', '-', filename).lower()
+    return sanitized_name
+
+
+def determine_evidence_type(filename):
+    # Determine evidence type
+    if re.match(env['VELOCIRAPTOR_EVIDENCE_PATTERN'], filename, re.IGNORECASE):
+        evidence_type = "VELOCIRAPTOR"
+        print("Velociraptor evidence")
+    else:
+        evidence_type = "other"    
+    return evidence_type
