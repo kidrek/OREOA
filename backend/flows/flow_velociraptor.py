d1b859910a43656ae297b14ddcf3c5ec5ac08cb6
kidrek <kidrek@gmail.com>
2024-08-31 23:10:09 +0200

    Add files via upload

--- backend/flows/flow_velociraptor.py
+++ backend/flows/flow_velociraptor.py
@@ -24,10 +24,18 @@ def run(input_path:str, output_path, sanitized_filename, password):
     # EVTX Analyse
     ## ZIRCOLITE
     tool_zircolite.zircolite_Windows(f'{output_path}/{sanitized_filename}.output', f'{output_path}/{sanitized_filename}.analyse')
+    ## Requirement : import dashboard in ElasticSearch
     tool_elasticsearch.send_data_to_elk('send_data_to_elk', directory_path=f"{output_path}/{sanitized_filename}.analyse/zircolite", index_name="zircolite", elasticsearch_host=env["ES_HOST"], elasticsearch_user=env["ES_USER"], elasticsearch_password=env["ES_PASSWORD"])
 
+    ## CHAINSAW
     tool_chainsaw.run(f'{output_path}/{sanitized_filename}.output', f'{output_path}/{sanitized_filename}.analyse')
+    ## Requirement : import dashboard in ElasticSearch
     tool_elasticsearch.send_data_to_elk('send_data_to_elk', directory_path=f"{output_path}/{sanitized_filename}.analyse/chainsaw", index_name="chainsaw", elasticsearch_host=env["ES_HOST"], elasticsearch_user=env["ES_USER"], elasticsearch_password=env["ES_PASSWORD"])
+
+    ## PLASO 
+    ### Log2timeline
+    tool_plaso.run_log2timeline(output_path, sanitized_filename)
+    tool_plaso.run_psort(f"{sanitized_filename}/plaso/plaso_log2timeline.plaso", sanitized_filename)
 
     #tool_elasticsearch.add_index_pattern_to_kibana('add_index_pattern_to_kibana', kibana_host=env['KIBANA_HOST'], index_prefix='zircolite*', index_name='zircolite_test')
 
