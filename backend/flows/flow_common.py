786c5a4a44cebba710fa5a3cac11ece10760413b
kidrek <kidrek@gmail.com>
2024-09-03 20:00:05 +0200

    Add files via upload

--- backend/flows/flow_common.py
+++ backend/flows/flow_common.py
@@ -1,21 +1,30 @@
 import logging
-from prefect import flow
-from tasks import utility
+from tasks import *
 
-@flow(name="common")
-def flow_common(original_path, original_filename, hash_algo):
+## Load variables from .env file
+from dotenv import load_dotenv
+from pathlib import Path
+from os import environ as env
+dotenv_path = Path('.env')
+load_dotenv(dotenv_path=dotenv_path)
+
+def run(input_path, analyse_path, sanitized_filename):
     logging.info('Flow Common : Starting')
 
-    # Sanitize name
-    sanitized_name = utility.sanitize_file_name(original_filename)
-    if original_filename != sanitized_name:
-        utility.move_file(f"{original_path}/{original_filename}", f"{original_path}/{sanitized_name}",)
+    # Antivirus Analyse
+    tool_clamav.run(input_path, analyse_path)
 
-    # Generate Hash
-    utility.generate_hash(hash=hash_algo, filepath=f"{original_path}/{sanitized_name}")
+    # EVTX Analyse
+    ## ZIRCOLITE
+    tool_zircolite.zircolite_Windows(input_path, analyse_path)
+    ## Requirement : import dashboard in ElasticSearch
+    tool_elasticsearch.send_data_to_elk('send_data_to_elk', directory_path=f"{analyse_path}/zircolite", index_name="zircolite", elasticsearch_host=env["ES_HOST"], elasticsearch_user=env["ES_USER"], elasticsearch_password=env["ES_PASSWORD"])
 
-    # Determine evidence type
-    evidence_type = utility.determine_evidence_type(original_filename)
+    ## CHAINSAW
+    tool_chainsaw.run(input_path, analyse_path)
+    ## Requirement : import dashboard in ElasticSearch
+    tool_elasticsearch.send_data_to_elk('send_data_to_elk', directory_path=f"{analyse_path}/chainsaw", index_name="chainsaw", elasticsearch_host=env["ES_HOST"], elasticsearch_user=env["ES_USER"], elasticsearch_password=env["ES_PASSWORD"])
 
+    #tool_elasticsearch.add_index_pattern_to_kibana('add_index_pattern_to_kibana', kibana_host=env['KIBANA_HOST'], index_prefix='zircolite*', index_name='zircolite_test')
 
-    return sanitized_name, evidence_type
+    ## Add timefield : matches.SystemTime / source : https://www.elastic.co/docs/api/doc/kibana/v8/operation/operation-createdataviewdefaultw#operation-createdataviewdefaultw-body-application-json-elastic-api-version-2023-10-31-data_view-timefieldname
