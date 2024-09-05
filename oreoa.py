f41650fefe33ced4d0383d2ce3f5721aa80a302f
kidrek <kidrek@gmail.com>
2024-09-05 11:23:03 +0200

    Add files via upload

--- oreoa.py
+++ oreoa.py
@@ -2,29 +2,14 @@ import argparse, logging, os
 from prefect import flow, task
 from prefect_dask.task_runners import DaskTaskRunner
 from backend.tasks import *
+from backend.flows import *
 
 ## Load variables from .env file
 from dotenv import load_dotenv
 from pathlib import Path
 from os import environ as env
-dotenv_path = Path('backend/.env')
+dotenv_path = Path('.env')
 load_dotenv(dotenv_path=dotenv_path)
-
-
-
-@flow(task_runner=DaskTaskRunner(cluster_kwargs={"processes": False}))
-def investigation_flow(input_evidence, output_analyse):
-
-    ## Analyse Antivirale
-    tool_clamav.run.submit(input_evidence, output_analyse)
-    
-    ## Analyse EVTX
-    tool_zircolite.run2Timesketch.submit(input_evidence, output_analyse)
-    tool_chainsaw.run.submit(input_evidence, output_analyse)
-
-    ## SuperTimeline
-    tool_plaso.run_log2timeline.submit(input_evidence, output_analyse)
-
 
 
 if __name__=="__main__":
@@ -36,19 +21,12 @@ if __name__=="__main__":
 
     parser.add_argument('-i', '--input_evidence')
     parser.add_argument('-o', '--output_analyse')
+    parser.add_argument('-c', '--case', default='case')
     args = parser.parse_args()
 
     input_evidence = args.input_evidence
     output_analyse = args.output_analyse
+    case = args.case
 
     ## Start - Processing Data
-    os.makedirs(f"{output_analyse}", exist_ok=True)
-    investigation_flow(input_evidence, output_analyse)
-
-    tool_plaso.run_psort2json(f"{output_analyse}/plaso/plaso_log2timeline.plaso", output_analyse)
-
-
-    ## Start - Sending results to ElasticSearch
-    if len(env['ES_HOST']) > 0:
-        for module in ['chainsaw','plaso', 'zircolite']:
-            tool_elasticsearch.send_data_to_elk('send_data_to_elk', directory_path=f"{output_analyse}/{module}", index_name=module, elasticsearch_host=env["ES_HOST"], elasticsearch_user=env["ES_USER"], elasticsearch_password=env["ES_PASSWORD"])
+    flow_common.run(input_evidence, output_analyse, case)
