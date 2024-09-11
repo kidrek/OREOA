a50265ab0de3faf61f96223a1c4179e28397ca84
kidrek <kidrek@gmail.com>
2024-09-11 15:05:27 +0200

    Add files via upload

--- backend/flows/flow_common.py
+++ backend/flows/flow_common.py
@@ -42,32 +42,46 @@ def run(input_path, analyse_path):
             if original_filename != sanitized_name:
                 utility.move_file(f"{original_path}/{original_filename}", f"{original_path}/{sanitized_name}",)
 
-    #        # Generate Hash
-    #        utility.generate_hash(hash=env['HASH_ALGO'], filepath=f"{original_path}/{sanitized_name}")
+            # Generate Hash
+            utility.generate_hash(hash=env['HASH_ALGO'], filepath=f"{original_path}/{sanitized_name}")
 
-    #        # Define output and analyse folders for this artifacts
-    #        scan_output_filename = f"{analyse_path}/output"
-    #        os.makedirs(f"{scan_output_filename}", exist_ok=True)
+            # Define output and analyse folders for this artifacts
+            scan_output_filename = f"{analyse_path}/output"
+            os.makedirs(f"{scan_output_filename}", exist_ok=True)
 
-    #        # Determine evidence type
-    #        evidence_type = utility.determine_evidence_type(original_filename)
+            # Determine evidence type
+            evidence_type = utility.determine_evidence_type(original_filename)
 
-    #        # Handle evidence type
-    #        if evidence_type == "velociraptor":
-    #            flow_evidence_velociraptor.run(input_path = f"{original_path}/{sanitized_name}", output_path = f"{scan_output_filename}", sanitized_filename = f"{sanitized_name}" , password = env['VELOCIRAPTOR_EVIDENCE_PASSWORD'])
+            # Handle evidence type
+            if evidence_type == "velociraptor":
+                flow_evidence_velociraptor.run(input_path = f"{original_path}/{sanitized_name}", output_path = f"{scan_output_filename}", sanitized_filename = f"{sanitized_name}" , password = env['VELOCIRAPTOR_EVIDENCE_PASSWORD'])
 
-    #            # Run - prefect flow / investigation_flow
-    #            flow_evidence_folder.investigate(scan_output_filename, analyse_output_filename)
+                # Run - prefect flow / investigation_flow
+                flow_evidence_folder.investigate(scan_output_filename, analyse_output_filename)
 
 
-    if os.path.isdir(env['TIMESKETCH_UPLOAD_PATH']) and os.path.isfile(f"{analyse_output_filename}/plaso/plaso_log2timeline.plaso"):
+    # Once tools finished, process results
+    process_result(sanitized_name, scan_output_filename, analyse_output_filename)
+
+
+@flow(task_runner=DaskTaskRunner(cluster_kwargs={"processes": False}))
+def process_result(sanitized_name, scan_output_filename, analyse_output_filename):
+    ### TODO - Création d'un workflow pour distribuer les tâches
+    if os.path.isdir(env['TIMESKETCH_UPLOAD_PATH']) :
 
         # Copy plaso file to TIMESKETCH_UPLOAD_PATH
-        utility.copy_file(f"{analyse_output_filename}/plaso/plaso_log2timeline.plaso", f"{env['TIMESKETCH_UPLOAD_PATH']}/{sanitized_name}.plaso")
+        if os.path.isfile(f"{analyse_output_filename}/plaso/plaso_log2timeline.plaso"):
+            utility.copy_file(f"{analyse_output_filename}/plaso/plaso_log2timeline.plaso", f"{env['TIMESKETCH_UPLOAD_PATH']}/{sanitized_name}.plaso")
+
+        # Copy hayabusa timeline to TIMESKETCH_UPLOAD_PATH
+        if os.path.isfile(f"{analyse_output_filename}/hayabusa/timesketch-import.csv"):
+            utility.copy_file(f"{analyse_output_filename}/hayabusa/timesketch-import.csv", f"{env['TIMESKETCH_UPLOAD_PATH']}/{sanitized_name}_hayabusa.csv")
+
 
         # TODO / Import plaso file with specific sketch name, and timeline name
         # set sketch as case_id
-        tool_timesketch.run_upload(sanitized_name)
+        tool_timesketch.run_upload.submit(f"{sanitized_name}.plaso")
+        tool_timesketch.run_upload.submit(f"{sanitized_name}_hayabusa.csv")
 
 
     # TODO / Generate HASH for all of analyses files
